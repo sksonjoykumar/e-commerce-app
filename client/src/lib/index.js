644f48eee6fd7dbd4ca0959c1bd416b7e0e92fcd
sksonjoykumar <sksonjoykumar111@gmail.com>
2024-10-22 20:08:32 +0600

    change

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -1,32 +1,7 @@
-// export const getData = async (endpoint) => {
-//   try {
-//     const response = await fetch(endpoint, {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Data fetching Error: " + response.statusText);
-//     }
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.log("Error while fetching data:", error);
-//     throw error;
-//   }
-// };
-
-import { config } from "./config"; // Import the configuration file
 
 export const getData = async (endpoint) => {
   try {
-    // Prepend the baseUrl from the config to the endpoint
-    const url = `${config.baseUrl}${endpoint}`;
-
-    const response = await fetch(url, {
+    const response = await fetch(endpoint, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
